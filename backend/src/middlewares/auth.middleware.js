246dbcf6085fb5d346b12f437049b3b0563c81fb
Prathamesh  Thakare <prathamthackeray18@gmail.com>
2026-02-08 17:51:04 +0530

    improved readability

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,30 +1,30 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 
-
-
-
-async function authUser(req,res,next){
+async function authUser(req, res, next) {
     const token = req.cookies.token;
 
-    if(!token){
-        return res.status(401).json({message: "unauthorized"});
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
     }
 
-    try{
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        const user = await userModel.findById(decoded._id)
+        const user = await userModel.findById(decoded.id);
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
 
         req.user = user;
-        next()
+        next();
 
-    } catch(error){
-        return res.status(401).json({message: "unauthorized"});
+    } catch (error) {
+        return res.status(401).json({ message: "Unauthorized" });
     }
 }
-
 
 module.exports = {
     authUser
-}
+};
