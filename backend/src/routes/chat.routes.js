851cf84b83e7e2edde44716b98f8e8439fa5fb72
Prathamesh  Thakare <prathamthackeray18@gmail.com>
2026-02-08 18:00:45 +0530

    added an  chat routes for creating chats and retrieving chat messages

--- backend/src/routes/chat.routes.js
+++ backend/src/routes/chat.routes.js
@@ -7,6 +7,13 @@ const chatController = require("../controllers/chat.controller");
 
 const router = express.Router();
 
+/* POST /api/chat/ */
 router.post("/", authMiddleware.authUser, chatController.createChat)
+
+/* GET /api/chat/ */
+router.get('/', authMiddleware.authUser, chatController.getChats)
+
+/* GET /api/chat/messages/:id */
+router.get('/messages/:id', authMiddleware.authUser, chatController.getMessages)
 
 module.exports = router;
