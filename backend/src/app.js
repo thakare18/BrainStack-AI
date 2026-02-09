abef96acdc7d2733a62e2df97680da7bb3ca0cea
Prathamesh  Thakare <prathamthackeray18@gmail.com>
2026-02-09 13:47:45 +0530

    cors middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //routes
 const authRoutes = require("./routes/auth.routes");
@@ -9,6 +10,7 @@ const chatRoutes = require("./routes/chat.routes");
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 //using routes
 app.use("/api/auth", authRoutes);
